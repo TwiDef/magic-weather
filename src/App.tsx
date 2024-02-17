c4238f388a559470d744510a057855155d5b2ad7
Def <deferenseo@gmail.com>
2024-02-17 16:08:42 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Reac, { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className='text-3xl font-bold underline'>text</p>
+    </>
+  )
+}
+
+export default App
